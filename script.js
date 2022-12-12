9d30ecf3135faa64e5fcf14676061ba39e553618
!duoduo123” <duorunfeng>
2022-12-12 22:49:22 +0900

    1st commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('button1').addEventListener('click', function() {
+    document.getElementById('button1').innerHTML = '안들리면 귀지를 파주세요,이 트릭이 효과가 없으면 병원에 가십시오';
+  });
+document.getElementById('button2').addEventListener('click', function() {
+    document.getElementById('button2').innerHTML = '눈에 이물질이 들어갔을 경우에는 재빨리 물로 씻어내고, 그래도 안되면 병원에 가서 약을 처방하십시오.';
+  });
+document.getElementById('button3').addEventListener('click', function() {
+    document.getElementById('button3').innerHTML = '코가 통하지 않으면 코를 푸는 것이 나을 수도 있고 그래도 안되면 약국에 가서 약을 사오세요.';
+  });
+document.getElementById('button4').addEventListener('click', function() {
+    document.getElementById('button4').innerHTML = '입 냄새는 주인이 고양이 양치질을 자주 안해서 그럴수도 있으니 양치질을 자주 해주세요';
+  });
+document.getElementById('button5').addEventListener('click', function() {
+    document.getElementById('button5').innerHTML = '나쁜 다리? 두 달 동안 석고에 넣으면 괜찮을 것입니다.';
+  });
+document.getElementById('button6').addEventListener('click', function() {
+    document.getElementById('button6').innerHTML = '꼬리가 안 흔들린다? 고양이가 당신에 대해 나쁜 인상을 가지고 있을지도 모릅니다';
+  });
+document.getElementById('button7').addEventListener('click', function() {
+    document.getElementById('button7').innerHTML = '허리 통증? 그녀에게 마사지를 해줘';
+  });
+document.getElementById('button8').addEventListener('click', function() {
+    document.getElementById('button8').innerHTML = '항상 설사를 하나요? 약을 먹고 차가운 발을 그녀의 뱃속에 두지 말고 따뜻하게 지내십시오.';
+  });//LjXaHVtU/vrZ0LHKlljxYZ2uh7rf1/6na9t3u/Ct70eyMrfHgeTwMxMfwYBuzV4Brnb10Mt5G2YI+AStPYRu6g==
